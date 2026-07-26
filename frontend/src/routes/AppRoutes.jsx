0ef168c96371536c2a3b4455aa021b35ed6822ed
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-07-26 17:25:55 +0530

    feat: update AppRoutes to include new head nurse components for drug management

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -9,21 +9,36 @@ import ManageUsers from "../pages/dr.tharaka/manageUsers";
 import ManageDepartments from "../pages/dr.tharaka/manageDepartments";
 import SummaryDrugsPage from "../pages/dr.tharaka/summaryDrugs";
 import SummaryInventoryPage from "../pages/dr.tharaka/summaryInventory";
+import DistributeDrugsPage from "../pages/dr.tharaka/distributeDrugs";
+import DistributeInventoryPage from "../pages/dr.tharaka/distributeInventory";
+import DMNDashbourd from "../pages/headNurse/drugsManageNurse/DMNDashbourd";
+import NurseCreateOrder from "../pages/headNurse/drugsManageNurse/NurseCreateOrder";
+import LowStocksTheaters from "../pages/headNurse/drugsManageNurse/LowStocksTheaters";
+import DistributeDrugs from "../pages/headNurse/drugsManageNurse/DistributeDrugs";
 
-export default function AppRoutes(){
-    return(
+export default function AppRoutes() {
+    return (
         <Routes>
-            <Route path="/*" element={<Login/> }/>
-            <Route path="/login" element={<Login/> }/>
-            <Route path="/dr.tharaka" element={<DrTharakaDashboard/> }/>
-            <Route path="/dr.tharaka/add-drugs" element={<AddDrugsStockPage/> }/>
-            <Route path="/dr.tharaka/add-inventory" element={<AddInventoryStockPage/> }/>
-            <Route path="/dr.tharaka/view-inventory-stock" element={<ViewInventoryStocks/> }/>
-            <Route path="/dr.tharaka/view-drugs-stock" element={<ViewDrugsStocks/> }/>
+            <Route path="/*" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/dr.tharaka" element={<DrTharakaDashboard />} />
+            <Route path="/dr.tharaka/add-drugs" element={<AddDrugsStockPage />} />
+            <Route path="/dr.tharaka/add-inventory" element={<AddInventoryStockPage />} />
+            <Route path="/dr.tharaka/view-inventory-stock" element={<ViewInventoryStocks />} />
+            <Route path="/dr.tharaka/view-drugs-stock" element={<ViewDrugsStocks />} />
             <Route path="/dr.tharaka/manage-departments" element={<ManageDepartments />} />
-            <Route path="/dr.tharaka/manage-users" element={<ManageUsers/>} />
-            <Route path="/dr.tharaka/summary-drugs" element={<SummaryDrugsPage/>} />
-            <Route path="/dr.tharaka/summary-inventory" element={<SummaryInventoryPage/>} />
+            <Route path="/dr.tharaka/manage-users" element={<ManageUsers />} />
+            <Route path="/dr.tharaka/summary-drugs" element={<SummaryDrugsPage />} />
+            <Route path="/dr.tharaka/summary-inventory" element={<SummaryInventoryPage />} />
+            <Route path="/dr.tharaka/distribute-drugs" element={<DistributeDrugsPage />} />
+            <Route path="/dr.tharaka/distribute-inventory" element={<DistributeInventoryPage />} />
+
+            <Route path="/hdm" element={<DMNDashbourd />} />
+            <Route path="/hdm/dashbourd" element={<DMNDashbourd />} />
+            <Route path="/hdm/create-order" element={<NurseCreateOrder />} />
+            <Route path="/hdm/low-stocks" element={<LowStocksTheaters />} />
+            <Route path="/hdm/distribute-drugs" element={<DistributeDrugs />} />
 
         </Routes>
     )
